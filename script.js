26955439d8f8d2c4f3708d4edb15f1a7a491a2ff
¨dim50dim¨ <zheliezkov@gmail.com>
2024-11-27 22:41:07 +0100

    file

---
+++ script.js
@@ -0,0 +1,9 @@
+function getRandomInt(a,b){
+    // let rand = max + Math.random() * (max + 1 - min);
+    console.log(a + b);
+    
+    return a + b
+     console.log(Math.floor(rand));
+}
+// console.log(getRandomInt(22,25));
+getRandomInt(22,26)
